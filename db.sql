f192127ccb3498b030fc68d17167123da9a8dd07
miracletanie <michelledian23@gmail.com>
2023-02-19 16:16:51 +0000

    first commit

---
+++ db.sql
@@ -0,0 +1,12 @@
+-- Q1 : What will be the OUTPUT of the following statement?
+-- reponse : il aura zero donné ou ligne  à la sortie 
+
+
+-- Q2: What will be the OUTPUT of the following statement?
+-- reponse : on aura 2 resultat 
+
+-- Q3: What will be the OUTPUT of the following statement?
+ -- reponse : il aura zero  ligne ou valeur à la sortie 
+
+-- Q4 : What will be the OUTPUT of the following statement?
+-- reponse : on aura une ligne non null  dont la valeur est 2 ou id 2 aussi 
